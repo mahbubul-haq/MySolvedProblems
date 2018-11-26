2ced289098f726cd5df4fa1736adf88dc9762d1e
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-11-26 15:07:08 +0600

    Create 495B.cpp

---
+++ codeforces/495B.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<long,long>
+#define vi vector<int>
+#define vii vector<pii>
+#define vll vector<ll>
+#define MOD 1000000007
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define MAX 4e18
+#define ff(i,a,b) for(int i=(a);i<=(b);i++)
+#define fb(i,a,b) for(int i=(a);i>=(b);i--)
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+    int a,b; cin>>a>>b;
+
+    if(a<b) {
+        cout<<0<<endl;
+        return 0;
+    }
+    if(a==b) {
+        cout<<"infinity"<<endl;
+        return 0;
+
+    }
+
+    int ans=0;
+
+    ff(i,1,sqrt(a-b)){
+
+        if((a-b)%i==0){
+            if((a-b)/i==i&&i>b) ans++;
+            else {
+                if(i>b) ans++;
+                if(((a-b)/i)>b) ans++;
+            }
+        }
+
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
+
