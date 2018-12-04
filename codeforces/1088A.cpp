94db1cce7ffa3d8b443dc0a97e38c9d6feb1cc08
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-12-05 00:12:16 +0600

    Create 1088A.cpp

---
+++ codeforces/1088A.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<long,long>
+#define vi vector<int>
+#define vii vector<pii>
+#define vll vector<ll>
+#define MOD 1000000007
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define ff(i,a,b) for(int i=(a);i<=(b);i++)
+#define fb(i,a,b) for(int i=(a);i>=(b);i--)
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define debugx(x) cout<<"WTH! "<<x<<endl
+#define debug(x,y) cout<<x<<" "<<y<<endl
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int x;
+
+    cin>>x;
+
+    for(int i=1;i<=100;i++){
+
+        for(int j=i;j<=100;j++){
+
+            if(j%i==0&&j*i>x&&(j/i)<x){
+                cout<<j<<" "<<i<<endl;
+                return 0;
+            }
+
+        }
+
+    }
+    
+    cout<<"-1"<<endl;
+
+    return 0;
+}
