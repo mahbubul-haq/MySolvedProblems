c7b067dd3b1456a70e4aa5753ae96862759fb33c
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-12-10 04:09:34 +0600

    Create 1020.cpp

---
+++ Timus/1020.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<long,long>
+#define vi vector<int>
+#define vii vector<pii>
+#define vll vector<ll>
+#define MOD 1000000007
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define ff(i,a,b) for(int i=(a);i<=(b);i++)
+#define fb(i,a,b) for(int i=(a);i>=(b);i--)
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define debugx(x) cout<<"WTH! "<<x<<endl
+#define debug(x,y) cout<<x<<" "<<y<<endl
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n;
+    double radius;
+    cin>>n>>radius;
+
+    double ans=0;
+
+    double zerox,zeroy;
+    cin>>zerox>>zeroy;
+
+    double tempx=zerox,tempy=zeroy;
+
+    ff(i,1,n-1){
+
+        double nowx,nowy;
+        cin>>nowx>>nowy;
+
+        ans+=sqrt(dist2D(nowx,nowy,tempx,tempy));
+        tempx=nowx,tempy=nowy;
+
+    }
+
+    ans+=sqrt(dist2D(tempx,tempy,zerox,zeroy));
+    ans+=2*acos(-1.0)*radius;
+
+    cout<<fixed<<setprecision(2)<<ans<<endl;
+
+    return 0;
+}
