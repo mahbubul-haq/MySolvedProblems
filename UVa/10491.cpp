95543e394a901c15c5e6777cf8b0733020eb2707
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-12-17 21:25:39 +0600

    Create 10491.cpp

---
+++ UVa/10491.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vll vector<ll>
+#define MOD 1000000007
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define ff(i,a,b) for(int i=(a);i<=(b);i++)
+#define fb(i,a,b) for(int i=(a);i>=(b);i--)
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+ostream& operator<<(ostream &os,const pll &a){
+    os<<a.first<<" "<<a.second;
+}
+
+pll operator+(const pll &a, const ll &b){ return { a.first+b,a.second+b };}
+pll operator*(const pll &a,const ll &b){ return {a.first*b,a.second*b};}
+pll operator+(const pll &a,const pll &b){ return {a.first+b.first,a.second+b.second};}
+pll operator-(const pll &a,const pll &b){ return { a.first-b.first,a.second-b.second};}
+pll operator*(const pll &a,const pll &b){ return {a.first*b.first,a.second*b.second};}
+pll operator%(const pll &a,const pll &b){ return {a.first%b.first,a.second%b.second};}
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    double  acow,acar,ashow;
+    while(cin>>acow>>acar>>ashow){
+
+        double ans=acow*(acar/(acow-ashow-1.0+acar));
+        ans+=(acar*((acar-1.0)/(acow-ashow+acar-1)));
+        ans=ans/(acow+acar);
+        cout<<fixed<<setprecision(5)<<ans<<endl;
+
+    }
+
+    return 0;
+}
