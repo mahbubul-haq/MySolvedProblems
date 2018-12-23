479a0e49be2405cbb6bf6f9c46055540e2c6e297
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-12-24 00:22:16 +0600

    Create 1087A.cpp

---
+++ codeforces/1087A.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vll vector<ll>
+#define MOD 1000000007
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define ff(i,a,b) for(int i=(a);i<=(b);i++)
+#define fb(i,a,b) for(int i=(a);i>=(b);i--)
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+ostream& operator<<(ostream &os,const pll &a){
+    os<<a.first<<" "<<a.second;
+}
+
+pll operator+(const pll &a, const ll &b){ return { a.first+b,a.second+b };}
+pll operator*(const pll &a,const ll &b){ return {a.first*b,a.second*b};}
+pll operator+(const pll &a,const pll &b){ return {a.first+b.first,a.second+b.second};}
+pll operator-(const pll &a,const pll &b){ return { a.first-b.first,a.second-b.second};}
+pll operator*(const pll &a,const pll &b){ return {a.first*b.first,a.second*b.second};}
+pll operator%(const pll &a,const pll &b){ return {a.first%b.first,a.second%b.second};}
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    string input; cin>>input;
+
+    int siz=input.size();
+
+    int i=0,j=siz-1;
+
+
+    bool flag=true;
+    if(siz&1) flag=false;
+    else flag=true;
+
+    string ans;
+    int cnt=0;
+
+    if(siz<=2){
+        cout<<input<<endl;
+        return 0;
+    }
+
+    while(i<j){
+        if(flag){
+            ans+=input[j];
+            j--;
+            flag=false;
+            cnt++;
+        }
+        else{
+            ans+=input[i];
+            i++;
+            flag=true;
+            cnt++;
+        }
+
+        if(cnt==siz-2){
+            ans+=input[i+1],ans+=input[i];
+            break;
+        }
+    }
+
+    for(int i=0;i<siz;i++) cout<<ans[siz-1-i];
+    cout<<endl;
+
+    return 0;
+}
