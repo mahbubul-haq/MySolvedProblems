e5170f3a3587a1893ac6a614ce0e61e28c5088fd
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-12-29 15:38:53 +0600

    Create 1096B.cpp

---
+++ codeforces/1096B.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vll vector<ll>
+#define MOD 998244353
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+ostream& operator<<(ostream &os,const pll &a){
+    os<<a.first<<" "<<a.second;
+}
+
+pll operator+(const pll &a, const ll &b){ return { a.first+b,a.second+b };}
+pll operator*(const pll &a,const ll &b){ return {a.first*b,a.second*b};}
+pll operator+(const pll &a,const pll &b){ return {a.first+b.first,a.second+b.second};}
+pll operator-(const pll &a,const pll &b){ return { a.first-b.first,a.second-b.second};}
+pll operator*(const pll &a,const pll &b){ return {a.first*b.first,a.second*b.second};}
+pll operator%(const pll &a,const pll &b){ return {a.first%b.first,a.second%b.second};}
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n; cin>>n;
+    string s; cin>>s;
+    ll cnt1=1,cnt2=0;
+
+    for(int i=1;i<n;i++){
+        if(s[i]==s[0]){
+            cnt1++;
+        }else break;
+    }
+
+    for(int i=n-1;i>=0;i--){
+        if(s[i]==s[n-1]){
+            cnt2++;
+        }
+        else break;
+    }
+
+    if(s[0]==s[n-1]){
+        cout<<((cnt1+1)*(cnt2+1))%MOD<<endl;
+    }
+    else{
+        cout<<(cnt1+1+cnt2)%MOD<<endl;
+    }
+    
+
+    return 0;
+}
