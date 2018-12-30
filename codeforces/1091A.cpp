170279f85df12c4a9e15435ac28d2df268f07242
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2018-12-31 02:35:58 +0600

    Create 1091A.cpp

---
+++ codeforces/1091A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define MOD 1000000007
+#define EPS 1e-7
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+ostream& operator<<(ostream &os,const pll &a){
+    os<<a.first<<" "<<a.second;
+}
+
+pll operator+(const pll &a, const ll &b){ return { a.first+b,a.second+b };}
+pll operator*(const pll &a,const ll &b){ return {a.first*b,a.second*b};}
+pll operator+(const pll &a,const pll &b){ return {a.first+b.first,a.second+b.second};}
+pll operator-(const pll &a,const pll &b){ return { a.first-b.first,a.second-b.second};}
+pll operator*(const pll &a,const pll &b){ return {a.first*b.first,a.second*b.second};}
+pll operator%(const pll &a,const pll &b){ return {a.first%b.first,a.second%b.second};}
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int y,b,r; cin>>y>>b>>r;
+
+
+
+    for(int i=y;i>=1;i--){
+        if(b>i&&r>i+1){
+            cout<<3*i+3<<endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
