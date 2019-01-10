8c5c45e1476ff9fa93cd2530a2d3b1d8973b2ade
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-01-11 01:47:02 +0600

    Create 1102A.cpp

---
+++ codeforces/1102A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define MOD 998244353
+#define EPS 1e17
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e18
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+ostream& operator<<(ostream &os,const pll &a){
+    os<<a.first<<" "<<a.second;
+}
+
+pll operator+(const pll &a, const ll &b){ return { a.first+b,a.second+b };}
+pll operator*(const pll &a,const ll &b){ return {a.first*b,a.second*b};}
+pll operator+(const pll &a,const pll &b){ return {a.first+b.first,a.second+b.second};}
+pll operator-(const pll &a,const pll &b){ return { a.first-b.first,a.second-b.second};}
+pll operator*(const pll &a,const pll &b){ return {a.first*b.first,a.second*b.second};}
+pll operator%(const pll &a,const pll &b){ return {a.first%b.first,a.second%b.second};}
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    ll n; cin>>n;
+
+    if(n&1){
+        if(((n+1)/2*n)&1) cout<<1<<endl;
+        else cout<<0<<endl;
+    }
+    else{
+        if(((n/2)*(n+1))&1) cout<<1<<endl;
+        else cout<<0<<endl;
+    }
+
+    return 0;
+}
