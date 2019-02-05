29d4621cfe998648650b75134414df457fceb1da
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-06 02:06:35 +0600

    Create 414B.cpp

---
+++ codeforces/414B.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>02
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-8
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+//#define FIFO
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+
+class compare{
+public:
+    bool operator()(pii x,pii y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+ostream& operator<<(ostream &os,const pll &a){
+    os<<a.first<<" "<<a.second;
+}
+
+pll operator+(const pll &a, const ll &b){ return { a.first+b,a.second+b };}
+pll operator*(const pll &a,const ll &b){ return {a.first*b,a.second*b};}
+pll operator+(const pll &a,const pll &b){ return {a.first+b.first,a.second+b.second};}
+pll operator-(const pll &a,const pll &b){ return { a.first-b.first,a.second-b.second};}
+pll operator*(const pll &a,const pll &b){ return {a.first*b.first,a.second*b.second};}
+pll operator%(const pll &a,const pll &b){ return {a.first%b.first,a.second%b.second};}
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+ll dp[2002][2002];
+int n,k;
+
+ll ret(int id,int x){
+
+    if(id>=k){
+        return 1;
+    }
+
+    if(dp[id][x]!=-1) return dp[id][x];
+
+    ll times=0;
+    int now=x;
+
+    while(now<=n){
+        times=(times+ret(id+1,now))%MOD;
+        now+=x;
+    }
+
+   /// cout<<id<<" "<<x<<" "<<times<<endl;
+    return dp[id][x]=times;
+
+}
+
+int main()
+{
+#ifdef FIFO
+    FI;
+    FO;
+#endif
+    FASTIO;
+
+    mem(dp,-1);
+
+    cin>>n>>k;
+
+    //cout<<n<<" "<<k<<endl;
+
+    cout<<ret(0,1)<<endl;
+
+    return 0;
+}
