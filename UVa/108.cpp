a863095db476fac121b207ac46040c679e55f4ec
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-08 17:05:49 +0600

    Create 108.cpp

---
+++ UVa/108.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-8
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 1e9
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+//#define FIFO
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define fof(i,x,y) for(int i=x;i<=(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> x,pair<T,T> y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+int main()
+{
+#ifdef FIFO
+    FI;
+    FO;
+#endif
+    FASTIO;
+
+    int n;
+    cin>>n;
+
+    int ara[n+1][n+1];
+    mem(ara,0);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int x; cin>>x;
+            ara[i][j]+=x;
+
+            if(i>0) ara[i][j]+=ara[i-1][j];
+            if(j>0) ara[i][j]+=ara[i][j-1];
+
+            if(i>0&&j>0) ara[i][j]-=ara[i-1][j-1];
+        }
+    }
+
+    int maxvalue=-100000000;
+
+    for(int i=0;i<n;i++){
+
+        int tempvalue=0;
+
+        for(int j=i;j<n;j++){
+
+
+                for(int k=0;k<n;k++){
+
+                    for(int l=k;l<n;l++) {
+
+                        tempvalue = ara[j][l];
+                        if(i>0) tempvalue-=ara[i-1][l];
+                        if(k>0) tempvalue-=ara[j][k-1];
+                        if(i>0&&k>0) tempvalue+=ara[i-1][k-1];
+
+                        maxvalue=max(maxvalue,tempvalue);
+
+                }
+            }
+        }
+    }
+
+    cout<<maxvalue<<endl;
+
+    return 0;
+}
