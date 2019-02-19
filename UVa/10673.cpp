4a9c3c5359d14a9369af37b3a447a78a5b91954d
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-19 15:41:54 +0600

    Create 10673.cpp

---
+++ UVa/10673.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-8
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 100000000
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define fof(i,x,y) for(int i=x;i<=(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> x,pair<T,T> y){
+        if(x.second==y.second){
+            return x.first<y.first;
+        }
+        return x.second<y.second;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+ll gcd(ll a,ll b, ll &x,ll &y){
+    if(b==0){
+        x=1;
+        y=0;
+        return a;
+    }
+    ll x1,y1;
+    ll d=gcd(b,a%b,x1,y1);
+
+    x=y1;
+    y=x1-(a/b)*y1;
+    return d;
+
+}
+
+bool exgcd(ll a,ll b,ll c,ll &x,ll &y,ll &g){
+
+    g=gcd(a,b,x,y);
+
+    if(c%g){
+        return false;
+    }
+
+    x*=c/g;
+    y*=c/g;
+
+    return true;
+
+}
+
+
+
+int main()
+{
+    //FI;FO;
+    FASTIO;
+
+    int t; cin>>t;
+
+    while(t--){
+        ll x,k;
+        cin>>x>>k;
+        ll a=x/k,b=ceil(x*1.0/k),c=x;
+        ll y,g;
+        if(exgcd(a,b,c,x,y,g)){
+            cout<<x<<" "<<y<<endl;
+        }
+    }
+
+    return 0;
+}
