b15c9a87daba4cbaf0ddf26aadced58b81faecbb
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-23 23:45:29 +0600

    Create 1131F.cpp

---
+++ codeforces/1131F.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-8
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 100000000
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define fof(i,x,y) for(int i=x;i<(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> x,pair<T,T> y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+template<class T>
+void print(vector<T> vec){
+    fof(i,0,vec.size()) cout<<vec[i]<<" "; cout<<endl;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+int main()
+{
+    //FI;FO;
+    FASTIO;
+
+    int n; cin>>n;
+
+    deque<int> dq[n+5];
+    int ara[n+5];
+    mem(ara,-1);
+
+    int cnt=0;
+
+    for(int i=1;i<n;i++){
+        int u,v; cin>>u>>v;
+        if(ara[u]==-1&&ara[v]==-1){
+            cnt++;
+            ara[u]=ara[v]=cnt;
+            dq[cnt].push_back(u);
+            dq[cnt].push_back(v);
+        }
+        else if(ara[u]==-1){
+            int tempcnt=ara[v];
+            dq[tempcnt].push_back(u);
+            ara[u]=tempcnt;
+        }
+        else if(ara[v]==-1){
+            int tempcnt=ara[u];
+            dq[tempcnt].push_back(v);
+            ara[v]=tempcnt;
+        }
+        else{
+
+            int tempcnt=min(ara[v],ara[u]);
+            int tempcnt1=max(ara[v],ara[u]);
+
+            while(!dq[tempcnt1].empty()){
+                dq[tempcnt].push_back(dq[tempcnt1].front());
+                ara[dq[tempcnt1].front()]=tempcnt;
+                dq[tempcnt1].pop_front();
+            }
+        }
+
+    }
+
+    vi ans(n+5,-1);
+    int cn1=1;
+    while(!dq[1].empty()){
+        cout<<dq[1].front()<<" ";
+       // ans[dq[1].front()]=cn1++;
+        dq[1].pop_front();
+    }
+
+    
+
+    return 0;
+}
