d769b073106d4364282a232af4364a60735ebba7
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-23 23:42:40 +0600

    Create 1131B.cpp

---
+++ codeforces/1131B.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-8
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 100000000
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define fof(i,x,y) for(int i=x;i<(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> x,pair<T,T> y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+template<class T>
+void print(vector<T> vec){
+    fof(i,0,vec.size()) cout<<vec[i]<<" "; cout<<endl;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+int main()
+{
+    //FI;FO;
+    FASTIO;
+
+    int n; cin>>n;
+
+    vii vec;
+
+    for(int i=0;i<n;i++){
+        int a,b; cin>>a>>b;
+        vec.pb(mp(a,b));
+    }
+
+    sortv(vec);
+
+    int cnt=1;
+    bool flag=false;
+
+    ll maxi=0,mini=0;
+    map<pii,bool> myMap;
+    myMap[mp(0,0)]=true;
+
+    for(int i=0;i<n;i++){
+        pii x=vec[i];
+
+
+        ll tempmin=max(mini,maxi);
+        ll tempmax=min(x.ff,x.ss);
+
+        if(tempmax>=tempmin){
+            cnt+=tempmax-tempmin+1;
+            if(myMap[mp(tempmin,tempmin)]) cnt--;
+            myMap[mp(tempmin,tempmin)]=true;
+            myMap[mp(tempmax,tempmax)]=true;
+        }
+        mini=x.ss,maxi=x.ff;
+
+    }
+
+    cout<<cnt<<endl;
+
+
+
+    return 0;
+}
