db5deeea42323b9c73d06c82a6146cfad4bd7c1e
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-03-21 11:48:13 +0600

    Create 1141E.cpp

---
+++ codeforces/1141E.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define pii pair<int,int>
+#define PQ priority_queue
+#define ld long double
+#define ll long long
+#define pll pair<ll,ll>
+#define vb vector<bool>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-12
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 100000000
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define fof(i,x,y) for(int i=x;i<(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> &x,pair<T,T> &y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+template<class T>
+void print(vector<T> &vec){
+    fof(i,0,vec.size()) cout<<vec[i]<<" "; cout<<endl;
+}
+template<class T>
+void print(set<T> &s){
+    for(auto it: s) cout<<it<<" "; cout<<endl;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+int main()
+{
+    //FI;FO;
+    FastIO;
+
+    ll H,n;
+    cin>>H>>n;
+
+    ll ara[n+1];
+    ara[0]=0;
+    ll mini=0;
+
+    fof(i,1,n+1){
+        int x; cin>>x;
+
+        ara[i]=ara[i-1]+x;
+
+        mini=min(ara[i],mini);
+
+        if(ara[i]+H<=0){
+            cout<<i<<endl;
+            return 0;
+        }
+    }
+
+    //debug(mini);
+   // debug(ara[n]);
+
+    if(ara[n]+H<H){
+        ll ans;
+        ll cnt=(H-abs(mini))/abs(ara[n]);
+
+        ans=cnt*n;
+
+        H=H+cnt*ara[n];
+        //debug(H);
+
+        if(H<=0){
+            cout<<ans<<endl;
+            return 0;
+        }
+
+        while(true){
+            int x;
+
+            for(x=1;x<=n;x++){
+                H=H+ara[x]-ara[x-1];
+                ans++;
+
+                if(H<=0){
+                    cout<<ans<<endl;
+                    return 0;
+                }
+            }
+
+            if(H<=0){
+                cout<<ans<<endl;
+                return 0;
+            }
+        }
+
+
+    }
+    else{
+        cout<<"-1"<<endl;
+    }
+
+    return 0;
+}
