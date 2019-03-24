3a98cb448497c96c7ee0c07953eae828ace6e4f3
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-03-24 23:10:39 +0600

    Create NICEDAY.cpp

---
+++ SPOJ/NICEDAY.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define EPS 1e-12
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define ff first
+#define fof(i,x,y) for(int i=x;i<(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+#define INF 100000000
+#define ld long double
+#define ll long long
+#define mem(x,y) memset(x,y,sizeof x)
+#define mp make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PQ priority_queue
+#define pb push_back
+#define pib pair<int,bool>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define sfi(x) scanf("%d",&x);
+#define sfii(x,y) scanf("%d%d",&x,&y)
+#define sfiii(x,y,z) scanf("%d%d%d",&x,&y,&z)
+#define siz(x) (int)x.size()
+#define sortv(v) sort(v.begin(),v.end())
+#define ss second
+#define vb vector<bool>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> &x,pair<T,T> &y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+template<class T>
+void print(vector<T> &vec){
+    fof(i,0,vec.size()) cout<<vec[i].ff<<" "; cout<<endl;
+}
+template<class T>
+void print(set<T> &s){
+    for(auto it: s) cout<<it<<" "; cout<<endl;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+#define LSOne(a) (a&(-a))
+class FenwickTree{
+    vi ft;
+public:
+    FenwickTree(int n){
+        ft.assign(n+1,INT_MAX);
+    }
+
+    void update(int a,int val){
+        for(;a<siz(ft);a+=LSOne(a)){
+            ft[a]=min(ft[a],val);
+        }
+    }
+
+    int query(int a){
+        int val=INT_MAX;
+        for(;a;a-=LSOne(a)){
+            val=min(val,ft[a]);
+        }
+        return val;
+    }
+};
+
+int main()
+{
+    //FI;FO;
+    FastIO;
+
+    int t; cin>>t;
+
+    while(t--){
+        int n; cin>>n;
+        FenwickTree ft(n);
+
+        vector<pair<int ,pii> > vec;
+
+        fof(i,0,n){
+            int a,b,c;
+            cin>>a>>b>>c;
+            vec.pb(mp(a,mp(b,c)));
+        }
+
+        sortv(vec);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int val=ft.query(vec[i].ss.ff);
+
+            if(val>vec[i].ss.ss) cnt++;
+
+            ft.update(vec[i].ss.ff,vec[i].ss.ss);
+        }
+
+        cout<<cnt<<endl;
+
+    }
+
+    return 0;
+}
