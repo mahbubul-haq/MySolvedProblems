04442f46d368515ce7d3405a54282a09de478c92
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-05-01 15:49:59 +0600

    Create 864E(1).cpp

---
+++ codeforces/864E(1).cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dist2D(x1,y1,x2,y2)         ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2)   ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define EPS                         1e-12
+#define FastIO                      ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define FI                          freopen("in.txt","r",stdin)
+#define FO                          freopen("out.txt","w",stdout)
+#define fap(x)                      cout<<"WTH: "<<x<<endl
+#define ff                          first
+#define fof(i,x,y)                  for(int i=x;i<(int)y;i++)
+#define fob(i,x,y)                  for(int i=x;i>=(int)y;i--)
+#define INF                         100000
+#define ld                          long double
+#define ll                          long long
+#define mem(x,y)                    memset(x,y,sizeof x)
+#define mp                          make_pair
+#define msi                         map<string,int>
+#define mii                         map<int,int>
+#define mis                         map<int,string>
+#define MOD                         1000000007
+#define PI                          acos(-1.0)
+#define PQ                          priority_queue
+#define pb                          push_back
+#define pib                         pair<int,bool>
+#define pii                         pair<int,int>
+#define pll                         pair<ll,ll>
+#define sfi(x)                      scanf("%d",&x)
+#define sfii(x,y)                   scanf("%d%d",&x,&y)
+#define sfiii(x,y,z)                scanf("%d%d%d",&x,&y,&z)
+#define siz(x)                      (int)x.size()
+#define sortv(v)                    sort(v.begin(),v.end())
+#define ss                          second
+#define ull                         unsigned long long
+#define umsi                        unordered_map<string,int>
+#define umii                        unordered_map<int,int>
+#define umis                        unordered_map<int,string>
+#define vb                          vector<bool>
+#define vi                          vector<int>
+#define vvi                         vector<vi>
+#define vii                         vector<pii>
+#define vvii                        vector<vii>
+#define vll                         vector<ll>
+#define vpll                        vector<pll>
+
+template<class T> class compare{
+public:
+    bool operator()(pair<T,T> &x,pair<T,T> &y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T> ostream& operator<<(ostream &os,const pair<T,T> &a) { os<<a.ff<<" "<<a.ss; }
+template<class T> void print(vector<T> &vec){
+    for(int i=1;i<siz(vec);i++) cout<<vec[i]<<" ";cout<<endl;
+}
+template<class T> void print(set<T> &s) {
+    for(auto it: s) cout<<it<<" "; cout<<endl;
+}
+template<class T> void print(list<T> &lst) {
+    for(auto it: lst) cout<<it<<" "; cout<<endl;
+}
+template<class T> ll power(T a,int b){
+    ll po=1; while(b--) po*=a; return po;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================//
+
+vector<pair<pii,pii> > vec;
+int n;
+int dp[101][2001];
+
+int main()
+{
+    //FI;FO;
+    FastIO;
+
+    cin>>n;
+    vec.resize(n+1);
+
+    mem(dp,0);
+
+    for(int i=0;i<n;i++){
+        int t,d,p; cin>>t>>d>>p;
+        d--;
+        vec[i+1]=mp(mp(d,i+1),mp(t,p));
+    }
+
+    sort(vec.begin()+1,vec.end());
+
+    //for(int i=1;i<siz(vec);i++) cout<<vec[i].ff<<endl;
+
+    int maxi=vec[n].ff.ff;
+
+    //cout<<maxi<<endl;
+
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=maxi;j++){
+            if(j<vec[i].ss.ff){
+                dp[i][j]=dp[i-1][j];
+            }
+            else if(j>=vec[i].ss.ff and j<=vec[i].ff.ff){
+                dp[i][j]=max(vec[i].ss.ss+dp[i-1][j-vec[i].ss.ff],dp[i-1][j]);
+            }
+            else {
+                dp[i][j]=max(dp[i-1][j],dp[i][vec[i].ff.ff]);
+            }
+            //cout<<i<<" "<<j<<endl;
+            //fap(dp[i][j]);
+        }
+    }
+
+    cout<<dp[n][maxi]<<endl;
+
+    vi ans;
+
+    int i=n,j=maxi;
+
+    while(i>=1 and j>=1){
+        if(dp[i][j]==dp[i-1][j])i--;
+        else if(dp[i][j]==dp[i][j-1]) j--;
+        else{
+            ans.pb(vec[i].ff.ss);
+            i--;
+            j-=vec[i+1].ss.ff;
+        }
+    }
+
+    //sortv(ans);
+
+    cout<<siz(ans)<<endl;
+    for(int v=siz(ans)-1;v>-1;v--) cout<<ans[v]<<" ";
+
+    return 0;
+}
