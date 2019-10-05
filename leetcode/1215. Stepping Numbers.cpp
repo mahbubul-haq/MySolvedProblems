0514748aba7d4ec16f37f6e0618f61916e61ab75
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-10-05 22:39:30 +0600

    Create 1215. Stepping Numbers.cpp

---
+++ leetcode/1215. Stepping Numbers.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dist2D(x1, y1, x2, y2)            ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+#define dist3D(x1, y1, z1, x2, y2, z2)    ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2))
+#define EPS                         1e-16
+#define endl                        "\n"
+#define FastIO                      ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define FI                          freopen("in.txt", "r", stdin)
+#define FO                          freopen("out.txt", "w", stdout)
+#define fap(x)                      cout << "WTH: " << x << endl
+#define ff                          first
+#define INF              1000000000000000000
+#define ld                          long double
+#define ll                          long long
+#define mem(x,y)                    memset(x, y, sizeof x)
+#define mp                          make_pair
+#define msi                         map<string, int>
+#define mii                         map<int, int>
+#define mis                         map<int, string>
+#define MOD                         1000000007
+#define PI                          acos(-1.0)
+#define PQ                          priority_queue
+#define pb                          push_back
+#define pib                         pair<int, bool>
+#define pii                         pair<int, int>
+#define pll                         pair<ll, ll>
+#define sfi(x)                      scanf("%d", &x)
+#define sfii(x,y)                   scanf("%d %d", &x, &y)
+#define sfiii(x,y,z)                scanf("%d %d %d", &x, &y, &z)
+#define siz(x)                      (int)x.size()
+#define sortv(v)                    sort(v.begin(), v.end())
+#define ss                          second
+#define timeTaken                   endd = clock(); cout << (double) (endd - beginn) / CLOCKS_PER_SEC * 1000 << endl
+#define timeInit                    clock_t beginn = clock(), endd
+#define ull                         unsigned long long
+#define umsi                        unordered_map<string, int>
+#define umii                        unordered_map<int, int>
+#define umis                        unordered_map<int, string>
+#define vb                          vector<bool>
+#define vi                          vector<int>
+#define vvi                         vector<vi>
+#define vii                         vector<pii>
+#define vvii                        vector<vii>
+#define vll                         vector<ll>
+#define vvll                        vector<vll>
+#define vpll                        vector<pll>
+
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional>
+using namespace __gnu_pbds;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> ordered_set;
+
+//os.order_of_key(v): returns how many elements strictly less than v
+//os.find_by_order(k - 1): returns kth smallest element's iterator
+
+
+template<class T> class compare {
+public:
+    bool operator() (pair<T, T> &x, pair<T, T> &y) {
+        if (x.first == y.first) {
+            return x.ss > y.ss;
+        }
+        return x.ff > y.ff;
+    }
+};
+
+template<class T> ostream &operator<<(ostream &os, const pair<T, T> &a) { return os << a.ff << " " << a.ss; }
+ll power(ll a, int b) {
+    ll po = 1;
+    while (b) {
+        if (b & 1)
+            po *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return po;
+}
+
+template<class T> pair<T, T> operator+(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff + b.ff, a.ss + b.ss}; }
+template<class T> pair<T, T> operator-(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff - b.ff, a.ss - b.ss}; }
+template<class T> pair<T, T> operator*(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff * b.ff, a.ss * b.ss}; }
+template<class T> pair<T, T> operator%(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff % b.ff, a.ss % b.ss}; }
+template<class T, class U> pair<T, T> operator+(const pair<T, T> &a, const U &b) { return {a.ff + b, a.ss + b}; }
+template<class T, class U> pair<T, T> operator*(const pair<T, T> &a, const U &b) { return {a.ff * b, a.ss * b}; }
+
+ll Set(ll N, int pos) { return N = N | (1LL << pos); }
+int reset(int N, int pos){ return N = N & ~ (1 << pos); }
+bool check(ll N, int pos){ return (bool) (N & (1LL << pos)); }
+
+///=======================================template=======================================///
+
+class Solution {
+public:
+    vll ans;
+
+    void func(int cnt, int nowcnt, int last, ll val) {
+        if (cnt == nowcnt) {
+            ans.pb(val);
+        }
+        else {
+            int low = last - 1;
+            int high = last + 1;
+            if (low >= 0) {
+                func(cnt, nowcnt + 1, low, val * 10 + low);
+            }
+            if (high <= 9) {
+                func(cnt, nowcnt + 1, high, val * 10 + high);
+            }
+        }
+    }
+
+    vector<int> countSteppingNumbers(int low, int high) {
+        int cntl = 1, cnt2 = 1;
+        int tlow = low, thigh = high;
+        tlow /= 10;
+        thigh /= 10;
+
+        while (tlow > 0 or thigh > 0) {
+            if (tlow)
+                cntl++;
+            if(thigh)
+                cnt2++;
+            tlow /= 10;
+            thigh /= 10;
+        }
+       // cout << cntl << " " << cnt2 << endl;
+
+        for (int i = cntl; i <= cnt2; i++) {
+            if (i == 1)
+                func(1, 1, 0, 0);
+            for (int j = 1; j <= 9;j++) {
+                func(i, 1, j, j);
+            }
+        }
+        vi anss;
+        sortv(ans);
+        
+        for (auto it : ans) {
+            if (it >= low and it <= high) {
+                anss.pb(it);
+            }
+        }
+        return anss;
+    }
+};
