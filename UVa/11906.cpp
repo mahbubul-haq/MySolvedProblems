77c1ff540dfe4c18aa14dbc3147afa244defe930
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-11-25 09:32:10 +0600

    Create 11906.cpp

---
+++ UVa/11906.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <set>
+#include <complex>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <bitset>
+#include <queue>
+#include <chrono>
+#include <random>
+#include <typeinfo>
+#include <stack>
+#include <list>
+using namespace std;
+
+#define all(v)                      v.begin(), v.end()
+#define Chrono                      chrono::steady_clock::now().time_since_epoch().count()
+#define dist2D(x1, y1, x2, y2)            ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+#define dist3D(x1, y1, z1, x2, y2, z2)    ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2))
+#define EPS                         1e-15
+#define endl                        "\n"
+#define FastIO                      ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define FI                          freopen("in.txt", "r", stdin)
+#define FO                          freopen("out.txt", "w", stdout)
+#define ff                          first
+#define INF              1000000000000000000
+#define ld                          long double
+#define ll                          long long
+#define mem(x,y)                    memset(x, y, sizeof x)
+#define mp                          make_pair
+#define msi                         map<string, int>
+#define mii                         map<int, int>
+#define mis                         map<int, string>
+#define MOD                         1000000007
+#define PI                          acos(-1.0)
+#define PQ                          priority_queue
+#define pb                          push_back
+#define pib                         pair<int, bool>
+#define pii                         pair<int, int>
+#define pll                         pair<ll, ll>
+#define present(c, x)               ((c).find(x) != (c).end())
+#define sfi(x)                      scanf("%d", &x)
+#define sfii(x,y)                   scanf("%d %d", &x, &y)
+#define sfiii(x,y,z)                scanf("%d %d %d", &x, &y, &z)
+#define siz(x)                      (int)x.size()
+#define ss                          second
+#define timeTaken                   endd = clock(); cout << (double) (endd - beginn) / CLOCKS_PER_SEC * 1000 << endl
+#define timeInit                    clock_t beginn = clock(), endd
+#define ull                         unsigned long long
+#define umsi                        unordered_map<string, int>
+#define umii                        unordered_map<int, int>
+#define umis                        unordered_map<int, string>
+#define vb                          vector<bool>
+#define vi                          vector<int>
+#define vvi                         vector<vi>
+#define vii                         vector<pii>
+#define vvii                        vector<vii>
+#define vll                         vector<ll>
+#define vvll                        vector<vll>
+#define vpll                        vector<pll>
+#define bug(...)                    cerr << __PRETTY_FUNCTION__ << " - " << __LINE__ << " : (" << #__VA_ARGS__ << ") = ("; _Print(__VA_ARGS__);
+template<class T> void _Print(T &&x) { cerr << x << ")" << endl; }
+template<class T, class ...S> void _Print(T &&x, S &&...y) { cerr << x << ", "; _Print(y...); }
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional>
+using namespace __gnu_pbds;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> ordered_set;
+
+//os.order_of_key(v): returns how many elements strictly less than v
+//os.find_by_order(k - 1): returns kth smallest element's iterator
+*/
+
+template<class T> class compare {
+public:
+    bool operator() (pair<T, T> &x, pair<T, T> &y) {
+        if (x.first == y.first) {
+            return x.ss > y.ss;
+        }
+        return x.ff > y.ff;
+    }
+};
+
+template<class T> ostream &operator<<(ostream &os, const pair<T, T> &a) { return os << a.ff << " " << a.ss; }
+ll power(ll a, int b) {
+    ll po = 1;
+    while (b) {
+        if (b & 1)
+            po *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return po;
+}
+
+template<class T> pair<T, T> operator+(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff + b.ff, a.ss + b.ss}; }
+template<class T> pair<T, T> operator-(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff - b.ff, a.ss - b.ss}; }
+template<class T> pair<T, T> operator*(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff * b.ff, a.ss * b.ss}; }
+template<class T> pair<T, T> operator%(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff % b.ff, a.ss % b.ss}; }
+template<class T, class U> pair<T, T> operator+(const pair<T, T> &a, const U &b) { return {a.ff + b, a.ss + b}; }
+template<class T, class U> pair<T, T> operator*(const pair<T, T> &a, const U &b) { return {a.ff * b, a.ss * b}; }
+
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos){ return N = N & ~ (1 << pos); }
+bool check(int N, int pos){ return (bool) (N & (1 << pos)); }
+
+///=======================================template=======================================///
+
+int main() {
+
+    //FI; FO;
+    int t;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        int r, c, m, n, even = 0, odd = 0;
+        cin >> r >> c >> m >> n;
+        vvi vis(r, vi(c, 0));
+
+        int w;
+        cin >> w;
+        while (w--) {
+            int x, y;
+            cin >> x >> y;
+            vis[x][y] = 2;
+        }
+
+        if (m < n)
+            swap(m, n);
+        pii dir[8];
+        int sign = 1;
+        for (int i = 0; i < 4;) {
+            dir[i] = {sign * m, n};
+            dir[i + 1] = {sign * m, -n};
+            i += 2;
+            sign *= -1;
+        }
+        for (int i = 4; i < 8;) {
+            dir[i] = {n, sign * m};
+            dir[i + 1] = {-n, sign * m};
+            i += 2;
+            sign *= -1;
+        }
+
+        vii points;
+        points.pb({0, 0});
+        int to = ((m == n)? 4 : 8);
+        while (!points.empty()) {
+            pii p = points.back();
+            //bug(p.ff, p.ss);
+            points.pop_back();
+            vis[p.ff][p.ss] = 1;
+            int cnt = 0;
+            map<pii, bool> viss;
+            for (int i = 0; i < to; i++) {
+                int tox = p.ff + dir[i].ff, toy = p.ss + dir[i].ss;
+                if (tox >= 0 and tox < r and toy >= 0 and toy < c and vis[tox][toy] != 2 and viss[{tox, toy}] == 0) {
+                    viss[{tox, toy}] = 1;
+                    cnt++;
+                    if (vis[tox][toy] == 0) {
+                        points.pb({tox, toy});
+                        vis[tox][toy] = 1;
+                    }
+                }
+            }
+
+            if (cnt & 1)
+                odd++;
+            else even++;
+
+        }
+
+        cout << "Case " << tc << ": " << even << " " << odd << endl;
+    }
+
+    return 0;
+}
+
+
+
